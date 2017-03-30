3edc3107c392e084b6a17843a63d23f70d0feabe
madhusudhand <madhusudhan.dollu@gmail.com>
2017-03-30 20:26:52 +0530

    changes as per 1.0.0-beta.4

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 'use strict';
-const router = require('donode').Router;
 
-router.routes([
+module.exports = [
   { path: '/', method: 'GET', handler: 'UserController@get' },
   {
     path: '/hello',
@@ -30,44 +29,4 @@ router.routes([
   // { path: '/user/settings', method: 'GET', handler: 'user/UserSettingsController@get' },
 
   // { path: '/user', method: 'ALL', handler: 'UserController' }
-
-
-  // {
-  //   path: '/loadtest/get/{id1}',
-  //   method: 'GET',
-  //   handler: 'LoadTestController@get',
-  //   children: [
-  //     { path: '/something1/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something2/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something3/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something4/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something5/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something6/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something7/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something8/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something9/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //     { path: '/something10/{id2}', method: 'GET', handler: 'LoadTestController@get' },
-  //   ]
-  // },
-  //
-  // {
-  //   path: '/loadtest/post/{id1}',
-  //   method: 'POST',
-  //   handler: 'LoadTestController@post',
-  //   children: [
-  //     { path: '/something1/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something2/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something3/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something4/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something5/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something6/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something7/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something8/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something9/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //     { path: '/something10/{id2}', method: 'POST', handler: 'LoadTestController@post' },
-  //   ]
-  // }
-
-]);
-
-module.exports = router;
+];
